0ea6c8fe14988bf7c3fa10c3195ce1003d4b90be
jiunjiun <quietmes@gmail.com>
2014-01-18 14:35:57 +0800

    Create Skirt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skirt::Application.config.secret_key_base = '423188bdaa0635fdbaf5f816366d6aaba56747b8378c14b379d864b20c385845324f8d21348e4ab4815989f020e2e13f33e4da76c4b95ffbdf2769c6e8015715'
