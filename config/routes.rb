5f67d4611997591fdd3ea2cc15371f41318d8626
jiunjiun <quietmes@gmail.com>
2014-01-18 14:40:05 +0800

    Add controller index and setting index is root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Skirt::Application.routes.draw do
+  root "index#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
