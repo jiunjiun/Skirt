1f0118232967cfed05cbb37d387a7a31c133505b
jiunjiun <quietmes@gmail.com>
2014-01-21 20:54:19 +0800

    g controller index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,25 @@
 class IndexController < ApplicationController
   def index
+    @url = Url.new
+  end
+
+  def goto
+    redirect_to Url.verifyCode(params[:code])
+  end
+
+  def create
+    @url = Url.new(url_params)
+    if @url.save
+      @render_url = root_url + @url.code
+    else
+      @render_url = root_url + Url.where(:url=> @url.url).first.code
+    end
+    render :text=> @render_url
+
+  end
+
+  private
+  def url_params
+    params.require(:url).permit(:url)
   end
 end
